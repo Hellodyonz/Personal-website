8207f5021e0290b3f20c9c7cc111de3580d86493
Hellodyonz <hellodyonz@gmail.com>
2023-10-08 16:46:29 +0700

    done footer & create portofolio page

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ document.querySelectorAll('nav-link'.forEach(n => n.addEventListener('click'), (
     hamburger.classList.remove('active');
     navMenu.classList.remove('active');
 }))
+
+// transisi sertifikat
+
+const images = [
+    "/Img/sertif-skilvul.png",
+    "/Img/certificate-festida.jpg",
+    "/Img/sertif-adobe.png",
+    "/Img/certificate-festida2.jpg"
+];
+
+const imgElement = document.querySelector(".achievements-section img");
+let currentIndex = 0;
+
+function changeImage() {
+    currentIndex++;
+    if (currentIndex >= images.length) {
+        currentIndex = 0;
+    }
+    const imageUrl = images[currentIndex];
+    imgElement.src = imageUrl;
+}
+
+setInterval(changeImage, 3000); // Ganti gambar setiap 3 detik
